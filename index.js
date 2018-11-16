7b79f2c7b30ca225a7ea829d13502267a54a57aa
robertobuso <robertobuso@gmail.com>
2018-11-16 16:24:48 -0500

    no test

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// Code your solution here!
+printCharacters = (string) => {
+  if (string.length < 1) {
+    return true
+  } else {
+  console.log(string.charAt(0))
+  printCharacters(string.substring(1))
+  }
+}
 
+const newArr = []
+
+reverseString = (string) => {
+  newArr.push(string[string.length-1])
+  if (string.length > 1) {
+    reverseString(string.substr(0, string.length - 1))
+  }
+   return newArr.join('')
+}
